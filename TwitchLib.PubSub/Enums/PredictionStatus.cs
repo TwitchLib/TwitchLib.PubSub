ba8746c8081341d5a455c984480b5ab02041d689
JayJay1989BE <jonas@lateur.pro>
2020-12-22 12:13:01 +0100

    Add initial Predictions feature

---
+++ TwitchLib.PubSub/Enums/PredictionStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.PubSub.Enums
+{
+    public enum PredictionStatus
+    {
+        Resolved = -2,
+        ResolvePending = -1,
+        Locked = 0,
+        Active = 1,
+    }
+}
