7b402ce651386296764b1b59df92438d6a77b7b0
AoshiW <ondru@SHIRO-V3>
2023-10-03 20:13:52 +0200

    optimization, logging source generators

---
+++ TwitchLib.PubSub/Models/Request.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TwitchLib.PubSub.Models
+{
+    internal class Request
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("nonce")]
+        public string Nonce { get; set; }
+
+        [JsonProperty("data")]
+        public RequestData Data { get; set; }
+    }
+
+    internal class RequestData
+    {
+        [JsonProperty("topics")]
+        public List<string> Topics { get; set; }
+
+        [JsonProperty("auth_token", NullValueHandling = NullValueHandling.Ignore)]
+        public string AuthToken { get; set; }
+    }
+}
